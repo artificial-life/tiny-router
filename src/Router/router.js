71b10e636bd8c7a958e9eecabd5f024d230f33f3
artificial-life <proteus.protocol@gmail.com>
2016-04-04 04:04:15 +0600

    draft

---
+++ src/Router/router.js
@@ -0,0 +1,24 @@
+'use strict'
+
+class TinyRouter {
+  constructor() {
+    this.routes = {};
+  }
+  setDefaultCallback() {
+
+  }
+  addRoute(route, callback) {
+
+  }
+  removeRoute(route_id) {
+
+  }
+  removeAll() {
+
+  }
+  parse(event) {
+
+  }
+}
+
+module.exports = TinyRouter;
