130d3ddaa48e24e7d9953a6b84e4691266d332c8
artificial-life <proteus.protocol@gmail.com>
2016-04-04 12:47:38 +0600

    upd

--- src/Router/router.js
+++ src/Router/router.js
@@ -1,14 +1,34 @@
 'use strict'
 
+const default_delimiter = '.';
+const mask_chars = ['*', '#'];
+
 class TinyRouter {
-  constructor() {
+  constructor(params) {
+    this.delimiter = params.delimiter || default_delimiter;
     this.routes = {};
   }
-  setDefaultCallback() {
-
+  setDefaultCallback(callback) {
+    this.default_callback = callback;
   }
   addRoute(route, callback) {
+    if (!_.isString(route)) throw new Error('Route must be string');
 
+    let _callback = _.isFunction(callback) ? callback : this.default_callback;
+    let route_data = this.hasMask(route) ? route.split(this.delimiter) : route;
+
+    return route_id;
+  }
+  hasMask(route_string) {
+    let result = false;
+
+    _.forEach(mask_chars, (char) => {
+      if (~route_string.indexOf(char)) return true;
+      result = true;
+      return false;
+    });
+
+    return result;
   }
   removeRoute(route_id) {
 
